76dc56eb5477f06677f8f63e143bca12241a4a6b
ROb <borrob@me.com>
2013-11-30 15:26:33 +0100

    improving bins

--- 010_bins.sql
+++ 010_bins.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION create_bins(start real, stop real, spacing real, tabname text)
+CREATE OR REPLACE FUNCTION create_bins(start double precision, stop double precision, spacing double precision, tabname text)
 RETURNS VOID AS
 $BODY$
 BEGIN
@@ -6,10 +6,14 @@ BEGIN
 	EXECUTE 'CREATE TABLE '||$4|| ' AS
 		SELECT
 			generate_series(1,('||$2||' - '||$1||')/'||$3||'+2) AS bin,
-			' ||$1||' + '||$3||'*generate_series(0,('||$2||' - '||$1||')/'||$3||'+1) AS binval
+			' ||$1||' + '||$3||'*generate_series(0,('||$2||' - '||$1||')/'||$3||'+1)::double precision AS binval_low,
+			' ||$1||' + '||$3||'*generate_series(1,('||$2||' - '||$1||')/'||$3||'+2)::double precision AS binval_high
 	;';
+	EXECUTE 'UPDATE '||$4||' SET binval_low=''-Infinity'' WHERE bin=1;';
+	EXECUTE 'UPDATE '||$4||' SET binval_high=''Infinity'' WHERE bin='||($2 - $1)/$3+2||';';
 	EXECUTE 'CREATE UNIQUE INDEX '||REPLACE($4,'.','_')||'_bin_idx ON '||$4|| ' (bin);';
-	EXECUTE 'CREATE UNIQUE INDEX '||REPLACE($4,'.','_')||'_binval_idx ON '||$4|| ' (binval);';
+	EXECUTE 'CREATE UNIQUE INDEX '||REPLACE($4,'.','_')||'_binvallow_idx ON '||$4|| ' (binval_low);';
+	EXECUTE 'CREATE UNIQUE INDEX '||REPLACE($4,'.','_')||'_binvalhigh_idx ON '||$4|| ' (binval_high);';
 END
 $BODY$
 LANGUAGE plpgsql;
